ec1f4474ff3ad4d085c83a85e9f62221a924232f
christian <christianm.vera@gmail.com>
2019-05-01 13:57:31 -0300

    trytoaddNancy

--- Startup.cs
+++ Startup.cs
@@ -35,14 +35,16 @@ namespace practica_Back_end
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
+            //else
+            //{
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+                //app.UseHsts();
+            //}
 
-            app.UseHttpsRedirection();
-            app.UseMvc();
+            //app.UseHttpsRedirection();
+            //app.UseMvc();
+            app.UseOwin(b => b.UseNancy());
+
         }
     }
 }
