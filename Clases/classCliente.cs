87b37044f2634550b6639b8d18865ba65fcad51c
christian <christianm.vera@gmail.com>
2019-05-02 00:54:29 -0300

    clasecliente

--- Clases/classCliente.cs
+++ Clases/classCliente.cs
@@ -4,6 +4,17 @@ namespace Cliente
 {
        public class ClienteModulo : NancyModule
         {
-            
+           public long Id {get; set;}
+           public string Nombre { get; set;}
+           public string Apellido { get; set;}
+           public string Direccion { get; set;}
+           
+           public ClienteModulo(long id, string nombre, string apellido, string direccion)
+           {
+                   this.Id = id;
+                   this.Nombre = nombre;
+                   this.Apellido = apellido;
+                   this.Direccion = direccion;
+           }
         }
 }
