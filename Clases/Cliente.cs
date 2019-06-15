53087d3a3546b2209f5a1fe1e083bfed806d6720
christian <christianm.vera@gmail.com>
2019-06-15 17:04:14 -0300

    clienteCompleto

--- Clases/Cliente.cs
+++ Clases/Cliente.cs
@@ -1,7 +1,9 @@
+using System;
 //en este espacio declaro las clases que voy a utilizar
 namespace Clases
 {
         //clase cliente 
+        [Serializable]
        public class Cliente
         {
            public int Id {get; set;}
@@ -9,9 +11,10 @@ namespace Clases
            public string Apellido { get; set;}
            public string Direccion { get; set;}
 
-        //constructor para inicializar sin atributos
+        /*constructor para inicializar sin atributos */
            public Cliente(){}
-        //constructor para inicializar con atributos
+
+        /*constructor para inicializar con atributos*/
            public Cliente(int id, string nombre, string apellido, string direccion)
            {
                    this.Id = id;
@@ -19,6 +22,17 @@ namespace Clases
                    this.Apellido = apellido;
                    this.Direccion = direccion;
            }
-        //metodos set
+
+            /*Metodos get de los atributos*/
+            public int getId() { return Id; }
+            public String getNombre() { return Nombre; }
+            public String getApellido() { return Apellido; }
+            public String getDireccion() { return Direccion; }
+
+            /*Metodos set de los atributos*/
+            public void setId(int i) { Id = i; }
+            public void setNombre(string nom) { Nombre = nom; }
+            public void setApellido(string ape) { Apellido = ape; }
+            public void setDireccion(string dire) { Direccion = dire; }
         }
 }
--- modules/ListaClientes-service.cs
+++ modules/ListaClientes-service.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections.Generic;
 using Clases;
 
@@ -7,31 +8,88 @@ namespace practica_Back_end
     public class ListaClientesService
     {
         private List<Cliente> listaCliente;
-        public ListaClientesService(){
+        public ListaClientesService()
+        {
             //inicializo mi lista con 
             this.listaCliente = new List<Cliente>
             {
                 new Cliente(0,"Carlos","Frazua","Dorbenite 2568"),
-                new Cliente(1,"Ezequiel","Miravales","Claromeco 56")
+                new Cliente(1,"Ezequiel","Miravales","Claromeco 56"),
+                new Cliente(1,"Francesco","Elvar","Gamorra 2")
             };
         }
         //retorna la lista de clientes    
-        public List<Cliente> getClientes(){
-        return this.listaCliente;
-    }
+        public List<Cliente> getClientes()
+        {
+            return this.listaCliente;
+        }
         // genera una id nueva para un nuevo cliente
-        public int generateId(){
+        public int generateId()
+        {
             int ultimo = this.listaCliente.Count-1;
             ultimo += 1;
             return ultimo;
         }
-        public void addCliente(Cliente newCliente){
+        public void addCliente(Cliente newCliente)
+        {
             newCliente.Id = generateId();          
             this.listaCliente.Add(newCliente);            
         }
-        
-        public Cliente getCliente(int pos){            
-            return this.listaCliente[pos];
+
+        /* retorna un cliente si existe en el listado */
+        public Cliente findCliente(int id)
+        {
+            int posAct =0;
+            bool existe = false;
+            Cliente clienteAct = null;
+
+            while (posAct < listaCliente.Count && !existe)
+            {
+                if (listaCliente.ElementAt(posAct).Id.Equals(id))
+                {
+                    clienteAct = listaCliente.ElementAt(posAct);
+                    existe = true;
+                }
+                posAct++;
+            }
+            return clienteAct;
+        }
+        /* Devuelve un cliente de una determinada posicion, sino existe de vuelve null */
+        public Cliente getClienteById(int id)
+        {
+            return findCliente(id);            
+        }
+
+        /* elimina la lista de clientes */
+        public void delClientes(){
+            listaCliente.Clear();
+        }
+
+        /* Si existe, elimina el cliente con ese id */
+        public bool delClienteById(int id)
+        {
+            bool seBorro = false;
+            if(findCliente(id) != null)
+            {
+                listaCliente.RemoveAt(id);
+                seBorro = true;
+            }
+            return seBorro;
+        }
+
+        /* Se actualiza un cliente si existe, sino devuelve nulo */
+        public Cliente updateCliente(int id,string nvoNom, string nvoApe, string nvoDir)
+        {
+            Cliente clt = findCliente(id);
+
+            if (clt != null)
+            {
+                clt.setNombre(nvoNom);
+                clt.setApellido(nvoApe);
+                clt.setDireccion(nvoDir);
+            }
+
+            return clt;
         }
     }
 }
